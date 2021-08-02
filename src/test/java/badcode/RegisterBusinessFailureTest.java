e405816547a375eb6fddf38b589c45895fa89130
up1 <somkiat.p@gmail.com>
2021-08-02 16:20:53 +0700

    Better of structure test

--- src/test/java/badcode/RegisterBusinessFailureTest.java
+++ src/test/java/badcode/RegisterBusinessFailureTest.java
@@ -5,32 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class RegisterBusinessTest {
-
-    // Success cases
-    @Test
-    @DisplayName("สามารถบันทึก speaker ลง database ได้")
-    public void case07() {
-        RegisterBusiness registerBusiness = new RegisterBusiness();
-        Speaker speaker = new Speaker();
-        speaker.setFirstName("Somkiat");
-        speaker.setLastName("Pui");
-        speaker.setEmail("somkiat@gmail.com");
-        // DAO = Data Access Object
-        // DTO = Data Transfer Object
-        // BO = Business Object
-        // VO = Value Object
-        int id = registerBusiness.register(new SpeakerRepository() {
-            @Override
-            public Integer saveSpeaker(Speaker speaker) {
-                return 100;
-            }
-        }, speaker);
-        assertEquals(100, id);
-    }
-
-    // Fail cases
-
+public class RegisterBusinessFailureTest {
     @Test
     @DisplayName("Firstname ของ speaker มีค่าเป็น null จึงเกิด exception ขึ้นมา")
     public void case01() {

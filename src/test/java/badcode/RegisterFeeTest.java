3410f1761f48d5c1361352f9012ad36b4f9ed774
up1 <somkiat.p@gmail.com>
2021-08-02 16:39:43 +0700

    Testing with parameterized

--- src/test/java/badcode/RegisterFeeTest.java
+++ src/test/java/badcode/RegisterFeeTest.java
@@ -2,10 +2,28 @@ package badcode;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class RegisterFeeTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "0,500",
+            "1,500",
+            "2,250",
+            "3,250",
+            "4,100",
+            "5,100",
+            "9,50",
+            "10,0",
+    })
+    public void getFeewithDataTable(int experience, int expectedFee) {
+        RegisterBusiness registerBusiness = new RegisterBusiness();
+        assertEquals(expectedFee, registerBusiness.getFee(experience));
+    }
 
     @Test
     @DisplayName("ทดสอบการคำนวณค่า fee จากประสบการณ์การทำงาน")
